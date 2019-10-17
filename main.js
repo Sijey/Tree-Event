71b3d0c96dd73df6a1faaaef7a3edd38b3249915
Sijey <Popov040391@gmail.com>
2019-10-17 16:58:14 +0300

    Solution

---
+++ main.js
@@ -0,0 +1,18 @@
+for (let li of tree.querySelectorAll('li')) {
+    let span = document.createElement('span');
+    li.prepend(span);
+    span.append(span.nextSibling);
+
+    tree.onclick = (event) => {
+        if (event.target.tagName != 'SPAN') {
+            return
+        }
+
+        let childStore = event.target.parentNode.querySelector('ul');
+        if (!childStore) {
+            return
+        }
+
+        childStore.hidden = !childStore.hidden;
+    }
+}
