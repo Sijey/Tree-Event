237f8337793384120571642d9e154e4ddd81832b
Sijey <Popov040391@gmail.com>
2019-10-22 09:49:30 +0300

    Solution

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-for (let li of tree.querySelectorAll('li')) {
-    let span = document.createElement('span');
+const tree = document.querySelector('.tree');
+const lis = document.querySelectorAll('li');
+for (let li of lis) {
+    const span = document.createElement('span');
     li.prepend(span);
     span.append(span.nextSibling);
-
-    tree.onclick = (event) => {
-        if (event.target.tagName != 'SPAN') {
-            return;
-        }
-
-        let childStore = event.target.parentNode.querySelector('ul');
-        if (!childStore) {
-            return;
-        }
-
-        childStore.hidden = !childStore.hidden;
-    }
 }
+  tree.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'SPAN') {
+      return;
+    }
+
+    const childStore = event.target.parentNode.querySelector('ul');
+    if (!childStore) {
+      return;
+    }
+
+    childStore.hidden = !childStore.hidden;
+  });
